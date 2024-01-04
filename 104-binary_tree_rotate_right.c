5ebafe6bbcedfea3569990150e37d3c0bd1cb7e2
laabk <laabkdavid@gmail.com>
2024-01-04 06:00:09 -0800

    104-binary_tree_rotate_right.c

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -1,26 +1,26 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_left - this rotates the binary tree towards right
+ * binary_tree_rotate_right - this rotates the binary tree towards right
  * @tree: root node of the all tree
  * Return: pointer to the new node
  */
 
-binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
 	binary_tree_t *center;
 
-	if (tree == NULL || tree->right == NULL)
+	if (tree == NULL || tree->left == NULL)
 	{
 	return (NULL);
 	}
-	center = tree->right;
-	tree->right = center->left;
-	if (center->left != NULL)
+	center = tree->left;
+	tree->left = center->right;
+	if (center->right != NULL)
 	{
 	center->left->parent = tree;
 	}
-	center->left = tree;
+	center->right = tree;
 	center->parent = tree->parent;
 	tree->parent = center;
 	return (center);
